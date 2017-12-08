948259b4b005a0fae0e3b08f803bb10e8742a85c
Felix D Leal C <felixdlealc@gmail.com>
2017-12-08 20:52:52 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var path = require('path');
+var toHttps = require('express-to-https').basic;
+
+var app = express();
+
+// - - To redirect to HTTPS
+//app.use(toHttps);
+
+// uncomment after placing your favicon in /public
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+//app.use(logger('dev'));
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '/')));
+//app.use(express.static('uploads'));
+//app.use(fileUpload());
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
